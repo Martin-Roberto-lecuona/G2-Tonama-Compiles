a9fafdbf866232dbc149ddc3de65face81aba497
ignacio montovio <iganciomontovio@gmail.com>
2024-07-01 23:34:54 -0300

    Modificar lyc version.

--- run.bat
+++ run.bat
@@ -4,16 +4,17 @@ del resultado.txt
 flex Lexico.l
 bison -dyv Sintactico.y
 
-gcc.exe lex.yy.c y.tab.c -o lyc-compiler-2.0.0.exe
-lyc-compiler-2.0.0.exe test.txt > resultado.txt
+gcc.exe lex.yy.c y.tab.c -o lyc-compiler-3.0.0.exe
+lyc-compiler-3.0.0.exe test.txt > resultado.txt
 
 @echo off
-del lyc-compiler-2.0.0.exe
+del lyc-compiler-3.0.0.exe
 del lex.yy.c
 del y.tab.c
 del y.tab.h
 del y.output
 
 :: tasm Final.asm
+:: tasm numbers.asm
 :: tlink Final.obj numbers.obj
 :: Final.exe
